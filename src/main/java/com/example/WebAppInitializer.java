5ac9d59412ccd04afac10e4a9f5e133c09348a12
DMGamage <gamagemadhushan@gmail.com>
2023-03-18 01:48:06 +0530

    first commit

---
+++ src/main/java/com/example/WebAppInitializer.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    public WebAppInitializer() {
+        System.out.println("Web");
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
